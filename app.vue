02be09794a5bd9164cd36b007d403986f07c0007
Florian Schmidt <florian@f012.dev>
2022-05-13 10:05:13 +0200

    Minimal reproduction repo

---
+++ app.vue
@@ -0,0 +1,8 @@
+<template>
+  <div>
+    <TestComponent>
+      <div data-test>Test</div>
+      <div data-test>Test</div>
+    </TestComponent>
+  </div>
+</template>
---
+++ components/TestComponent.vue
@@ -0,0 +1,15 @@
+<template>
+  <template v-for="(vnode, index) in vnodes" :key="index">
+    <component :is="vnode"></component><br>
+  </template>
+</template>
+
+<script lang="ts" setup>
+import { computed, useSlots } from 'vue';
+const vnodes = computed(() => {
+  console.log("update");
+  const defaultSlot = useSlots().default();
+  return defaultSlot.filter((vnode) => "data-test" in vnode.props);
+});
+
+</script>
